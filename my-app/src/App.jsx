a1a622925a1dcf9fb6cd8ea6482ec8d47890cbb7
YooJongHyeon <justin77748292@gmail.com>
2022-04-23 03:49:54 +0900

    cleanup the end

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-  return <div></div>;
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  useEffect(() => {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
 }
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
 export default App;
