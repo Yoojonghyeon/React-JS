86ae920c61a94bc3a5b3db10e1f0d58a6ae54332
YooJongHyeon <justin77748292@gmail.com>
2022-04-27 18:40:35 +0900

    git pages save

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -5,7 +5,8 @@ function Detail() {
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
+    ) //${id}
+      .json();
     console.log(json);
   };
   useEffect(() => {
